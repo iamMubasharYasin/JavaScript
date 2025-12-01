f46417364a781e752bd48ee444e8b22c91702263
Mubashar Yasin <mubasharraj012@gmail.com>
2025-12-01 12:41:21 +0500

    understanding variables

---
+++ 01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "mubasharyasin@gmail.com"
+var accountPassword = "12348"
+accountCity = "Kotli"
+//accountState
+
+//accountId = 2 //not allowed
+accountEmail = "example@gmail.com"
+accountPassword = "0000"
+accountCity = "Khuritta"
+
+//console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
+
